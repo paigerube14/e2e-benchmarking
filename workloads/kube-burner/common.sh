3a62ade3bae211152915db1224d690925c8b24e3
Paige Rubendall <prubenda@redhat.com>
2021-11-01 17:57:07 -0400

    adding make deploy back

--- workloads/kube-burner/common.sh
+++ workloads/kube-burner/common.sh
@@ -30,7 +30,7 @@ deploy_operator() {
   log "Cloning benchmark-operator from branch ${OPERATOR_REPO} of ${OPERATOR_REPO}"
   rm -rf benchmark-operator
   git clone --single-branch --branch ${OPERATOR_BRANCH} ${OPERATOR_REPO} --depth 1
-  (cd benchmark-operator && make deploy IMG=quay.io/prubenda/benchmark-operator:write-to-file)
+  (cd benchmark-operator && make deploy)
   kubectl apply -f benchmark-operator/resources/backpack_role.yaml
   kubectl apply -f benchmark-operator/resources/kube-burner-role.yml
   log "Waiting for benchmark-operator to be running"
