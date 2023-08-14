ceada5e0434205fec01f18df264edbfc870251e5
Paige Rubendall <prubenda@redhat.com>
2023-08-14 17:26:18 -0400

    adding +a

--- workloads/kube-burner-ocp-wrapper/run.sh
+++ workloads/kube-burner-ocp-wrapper/run.sh
@@ -2,7 +2,8 @@
 
 set -e
 
-ES_SERVER=${ES_SERVER=https://search-perfscale-dev-chmf5l4sh66lvxbnadi4bznl3a.us-west-2.es.amazonaws.com}
+set +a 
+ES_SERVER=${ES_SERVER:-https://search-perfscale-dev-chmf5l4sh66lvxbnadi4bznl3a.us-west-2.es.amazonaws.com}
 LOG_LEVEL=${LOG_LEVEL:-info}
 if [ "$KUBE_BURNER_VERSION" = "default" ]; then
     unset KUBE_BURNER_VERSION
