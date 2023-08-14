3fa12a8c4b483de22ce113694e56d55ede6363cf
Paige Rubendall <prubenda@redhat.com>
2023-08-14 17:28:15 -0400

    -

--- workloads/kube-burner-ocp-wrapper/run.sh
+++ workloads/kube-burner-ocp-wrapper/run.sh
@@ -1,8 +1,7 @@
 #!/bin/bash -e
 
-set -e
+set -ae
 
-set +a 
 ES_SERVER=${ES_SERVER:-https://search-perfscale-dev-chmf5l4sh66lvxbnadi4bznl3a.us-west-2.es.amazonaws.com}
 LOG_LEVEL=${LOG_LEVEL:-info}
 if [ "$KUBE_BURNER_VERSION" = "default" ]; then
